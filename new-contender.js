9218d9b33259853616e947d7926308c259890a9d
Daniel Calamateo <dany.calamateo@gmail.com>
2022-01-13 19:56:28 -0600

    first commit

---
+++ new-contender.js
@@ -0,0 +1,12 @@
+var añoNacimiento = 1997;
+var añoFuturo = 2050;
+var edad = añoFuturo - añoNacimiento;
+
+console.log("Si naciste en " + añoNacimiento + ", en " + añoFuturo + " tendrás " + (edad - 1) + " o " + edad + " años dependiendo del mes.");
+
+
+var nombre = "Jonh";
+var ubicacion = "Canada";
+var hobby = "programar";
+
+console.log("Hola, mi nombre es " + nombre + ". Vivo en " + ubicacion + " y me gusta " + hobby + ".")
